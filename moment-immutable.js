c20effe8b6c960945bdcd773914b1cda978a8066
Smartin Nematronic <smartin.negele@gmail.com>
2017-09-22 18:11:14 +0200

    updated dependencies

--- moment-immutable.js
+++ moment-immutable.js
@@ -9,7 +9,7 @@
 		root.moment = factory(root.moment);                     // Browser
 	}
 }(this, function (moment) {
-	var VERSION = '1.0.2',
+	var VERSION = '1.0.3',
 		momentMutablesAndParameters = {
 			'add': 0,
 			'endOf': 0,
